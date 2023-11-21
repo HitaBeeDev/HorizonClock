d934bac0bd09b28af93183ac46a7f7f1155ca29d
Anahita A <126061044+HitaBeeDev@users.noreply.github.com>
2023-11-21 23:39:01 +0300

    Update Clock.jsx

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -6,12 +6,12 @@ import numberPositions from "./numberPositions";
 export default function Clock({ hours, minutes, seconds }) {
   return (
     <div
-      className="shadow-lg	shadow-slate-300	border-4	border-slate-300 w-screen h-screen relative w-52 h-52 rounded-full bg-slate-200
+      className="w-52 h-52 shadow-lg	shadow-slate-300	border-4	border-slate-300 relative rounded-full bg-slate-200
    flex items-center justify-center"
     >
       {numberPositions.map(({ num, rotation }) => (
         <h2
-          className="absolute w-40 h-40 origin-center transform text-white text-sm flex items-center justify-center text-slate-950"
+          className="absolute w-40 h-40 origin-center transform text-white text-sm flex items-center justify-center text-slate-900"
           style={{
             transform: `rotate(${rotation}deg) translate(-50%) rotate(${-rotation}deg)`,
           }}
